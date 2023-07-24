ab9c31601bc89897878f9c5504e6c4f99f754f90
Vendela <vendela.o@hotmail.com>
2023-07-24 16:11:09 +0200

    feat: user directed to catalogue page when logged in

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,7 +10,7 @@ const routes: Routes = [
   {
       path:'',
       pathMatch:"full",
-      component: LandingPageComponent,
+      component: LandingPageComponent
   },
   {
       path:'pokemon-catalogue',
--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -12,29 +12,12 @@ export class AuthGuard {
     state: RouterStateSnapshot
   ): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree => {
 
-    console.log("hej")
-
     if (localStorage.getItem('username') !== null) {
       return true;
     } else {
-      // Redirect to the login page or any other route when the user is not logged in
+      // Redirect to the login page  when the user is not logged in
       return false;
       
     }
   }
 }
-
-/*export class AuthGuard implements CanActivateFn {
-
-  canActivate(
-    route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-
-     if(localStorage.getItem('username') != "not logged"){
-      return true
-     }
-
-    return false;
-  }
-  
-}*/
