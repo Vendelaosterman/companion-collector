d3687cabbdd7b07b2ba94bf8254c1a1263022d6a
Vendela <vendela.o@hotmail.com>
2023-07-24 08:41:39 +0200

    structure: page components created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { LoginPageComponent } from "./pages/login-page/login-page.component";
+import { LandingPageComponent } from "./pages/landing-page/landing-page.component";
+import { PokemonCataloguePageComponent } from "./pages/pokemon-catalogue-page/pokemon-catalogue-page.component";
+import { AuthGuard } from "./guards/auth.guard";
+
+const routes: Routes = [
+  {
+      path:'',
+      pathMatch:"full",
+      component:LoginPageComponent,
+  },
+  {
+      path:'landing',
+      component:LandingPageComponent,
+      //canActivate:[AuthGuard]
+  },
+  {
+      path:'pokemon-catalogue',
+      component:PokemonCataloguePageComponent,
+      //canActivate:[AuthGuard]
+  }
+]
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { LandingPageComponent } from './pages/landing-page/landing-page.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPageComponent,
+    LandingPageComponent,
+
   ],
   imports: [
     BrowserModule,
@@ -16,3 +22,4 @@ import { AppComponent } from './app.component';
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
