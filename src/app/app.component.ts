ac0d6c197e44176a003a8aa43348194f5a92189f
Vendela <vendela.o@hotmail.com>
2023-07-24 21:25:54 +0200

    feat: nav is visible when logged in

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,23 @@
 import { Component } from '@angular/core';
+import { Router, NavigationEnd } from "@angular/router";
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
   title = 'companion-collector';
   isLoggedIn: boolean = false;
 
+  constructor(private readonly router: Router) {}
+
   ngOnInit(){
-    this.checkUserLoggedIn();
+    this.router.events
+      .pipe(filter((event) => event instanceof NavigationEnd))
+      .subscribe(() => this.checkUserLoggedIn());
   }
 
   checkUserLoggedIn(){
