7a2da8e66cefc5d75fc132e21124eb9a35e87f41
emmahogstrand <emmahogstrand@gmail.com>
2023-07-25 09:44:10 +0200

    feature: pokemon list, services folder

--- src/app/components/pokemon-list/pokemon-list.component.ts
+++ src/app/components/pokemon-list/pokemon-list.component.ts
@@ -12,12 +12,14 @@ export class PokemonListComponent implements OnInit {
   public pokemons:Pokemon[] = [{
     id: 1,
     name: "pikatuch",
-    img: "http://localhost:3000/assets/images/1.jpg",
+    ability: "" ,
+    sprite: "https://github.com/PokeAPI/sprites/raw/master/sprites/pokemon/versions/generation-iv/heartgold-soulsilver/25.png",
   },
   {
     id: 2,
-    name: "Ashcethcup",
-    img: "http://localhost:3000/assets/images/2.jpg",
+    name: "Bulbasaur",
+    ability: "",
+    sprite: "https://th.bing.com/th/id/R.fda75fcc5e87c54e8e41cb95f9feed59?rik=KGxCEZErC6sUXw&riu=http%3a%2f%2fwww.pngmart.com%2ffiles%2f11%2fBulbasaur-PNG-Transparent-Image.png&ehk=iPUr8kInH6ju1PrWo1ow3GWjkoyCOvPumhqPvYQyw5s%3d&risl=&pid=ImgRaw&r=0",
   }]
 
   constructor(private readonly router:Router) { }
